66c93e7501b940351fec10f3e912bf3fd8eca0cb
siimtina <siim@fookus.eu>
2020-09-09 21:30:48 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,4 +3,10 @@ const path = require('path')
 
 const app = express()
 
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/*', (request, response) => {
+    response.sendFile(pathToIndex);
+});
+
 module.exports = app;
